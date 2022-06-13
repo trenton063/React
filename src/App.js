929ea00785ca389a25ef098c4d1db5e0f3fb5c85
Trenton <050trenton@gmail.com>
2022-06-13 14:39:23 -0400

    adding input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Expenses from "./components/Expenses";
+import React from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -8,7 +11,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -24,10 +32,10 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}></Expenses>
     </div>
   );
-}
+};
 
 export default App;
